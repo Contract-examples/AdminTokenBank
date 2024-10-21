408d3fd8f5ff1f0aeb60abd1d5213ed7ef2c370c
gmh5225 <2315157@qq.com>
2024-10-21 15:39:57 +0800

    update

--- src/AdminTokenBank.sol
+++ src/AdminTokenBank.sol
@@ -3,29 +3,37 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 
-contract TokenBank {
+contract AdminTokenBank is Ownable {
     using SafeERC20 for IERC20;
+
+    // admin
+    address public admin;
 
     // token
     IERC20 public token;
-
-    // user => balance
-    mapping(address => uint256) public balances;
+    // balance
+    uint256 public balances;
 
     // error
     error DepositTooLow();
     error InsufficientBalance();
     error TransferFailedForDeposit();
     error TransferFailedForWithdraw();
+    error OnlyAdminOrOwnerCanWithdraw();
+    error WithdrawOnlyToAdminOrOwner();
+    error InvalidAdminAddress();
 
     // event
     event Deposit(address indexed user, uint256 amount);
     event Withdraw(address indexed user, uint256 amount);
+    event AdminChanged(address indexed oldAdmin, address indexed newAdmin);
 
-    constructor(address _token) {
+    constructor(address _admin, address _token) Ownable(msg.sender) {
         token = IERC20(_token);
+        admin = _admin;
     }
 
     function deposit(uint256 amount) public {
@@ -35,38 +43,61 @@ contract TokenBank {
         }
 
         // transfer token from user to contract (safe transfer)
-        token.safeTransferFrom(msg.sender, address(this), amount);
+        token.safeTransferFrom(_msgSender(), address(this), amount);
 
         // update balance
-        balances[msg.sender] += amount;
+        balances += amount;
 
         // emit event
-        emit Deposit(msg.sender, amount);
+        emit Deposit(_msgSender(), amount);
     }
 
-    function withdraw(uint256 amount) external {
-        // if amount > balance, revert
-        if (amount > balances[msg.sender]) {
+    function withdrawTo(uint256 amount, address to) external {
+        // if msg.sender is not admin or owner, revert
+        if (_msgSender() != admin && _msgSender() != owner()) {
+            revert OnlyAdminOrOwnerCanWithdraw();
+        }
+
+        // if to is not admin or owner, revert
+        if (to != admin && to != owner()) {
+            revert WithdrawOnlyToAdminOrOwner();
+        }
+
+        // if amount is greater than balance, revert
+        if (amount > balances) {
             revert InsufficientBalance();
         }
 
         // transfer token from contract to user (safe transfer)
-        token.safeTransfer(msg.sender, amount);
+        token.safeTransfer(to, amount);
 
         // update balance
-        balances[msg.sender] -= amount;
+        balances -= amount;
 
         // emit event
-        emit Withdraw(msg.sender, amount);
+        emit Withdraw(to, amount);
     }
 
-    // Query contract balance
-    function getBalance() public view returns (uint256) {
-        return token.balanceOf(address(this));
+    function transferAdmin(address newAdmin) external onlyOwner {
+        // if newAdmin is 0, revert
+        if (newAdmin == address(0)) {
+            revert InvalidAdminAddress();
+        }
+
+        // update admin
+        address oldAdmin = admin;
+        admin = newAdmin;
+
+        // emit event
+        emit AdminChanged(oldAdmin, newAdmin);
     }
 
-    // Query specific depositor's deposit amount
-    function getDepositAmount(address depositor) public view returns (uint256) {
-        return balances[depositor];
+    function renounceAdmin() external onlyOwner {
+        // update admin
+        address oldAdmin = admin;
+        admin = address(0);
+
+        // emit event
+        emit AdminChanged(oldAdmin, address(0));
     }
 }
--- test/AdminTokenBank.t.sol
+++ test/AdminTokenBank.t.sol
@@ -2,47 +2,41 @@
 pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
-import "../src/TokenBank.sol";
-import "../src/SimpleToken.sol";
+import "../src/AdminTokenBank.sol";
+import "../src/MyERC20Token.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract TokenBankTest is Test {
-    TokenBank public bank;
-    SimpleToken public token;
-    address public user1;
-    address public user2;
-    address public user3;
+contract AdminTokenBankTest is Test {
+    AdminTokenBank public bank;
+    MyERC20Token public token;
+    address public owner;
+    address public admin;
+    address public user;
 
     function setUp() public {
-        token = new SimpleToken(1_000_000 * 10 ** 18); // 1,000,000 tokens
-        bank = new TokenBank(address(token));
-        user1 = address(0x1);
-        user2 = address(0x2);
-        user3 = address(0x3);
+        token = new MyERC20Token("MyNFTToken", "MTK");
+        owner = address(this);
+        admin = address(0x260ef6029160BB9E89A50e0ead94DBf3F4725106);
+        user = address(0x1);
+        bank = new AdminTokenBank(admin, address(token));
 
-        // transfer token to user1, user2, user3
-        token.transfer(user1, 1000 * 10 ** 18);
-        token.transfer(user2, 2000 * 10 ** 18);
-        token.transfer(user3, 3000 * 10 ** 18);
+        // give user 1000 tokens
+        token.mint(user, 1000 * 10 ** 18);
     }
 
-    function testDeposit() public {
-        vm.startPrank(user1);
+    function testDepositAndWithdraw() public {
+        // deposit by user
+        vm.startPrank(user);
         token.approve(address(bank), 500 * 10 ** 18);
         bank.deposit(500 * 10 ** 18);
         vm.stopPrank();
 
-        assertEq(bank.balances(user1), 500 * 10 ** 18);
-    }
+        assertEq(bank.balances(), 500 * 10 ** 18);
 
-    function testWithdraw() public {
-        vm.startPrank(user1);
-        token.approve(address(bank), 500 * 10 ** 18);
-        bank.deposit(500 * 10 ** 18);
-        uint256 initialBalance = token.balanceOf(user1);
-        bank.withdraw(250 * 10 ** 18);
+        vm.startPrank(user);
+        // set expect revert
+        vm.expectRevert(AdminTokenBank.OnlyAdminOrOwnerCanWithdraw.selector);
+        bank.withdrawTo(500 * 10 ** 18, user);
         vm.stopPrank();
-
-        assertEq(bank.balances(user1), 250 * 10 ** 18);
-        assertEq(token.balanceOf(user1), initialBalance + 250 * 10 ** 18);
     }
 }
