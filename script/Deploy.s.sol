fcba0241bc40a78d882871bddcdbc3698334446a
gmh5225 <2315157@qq.com>
2024-10-21 16:36:47 +0800

    update

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -10,7 +10,7 @@ contract DeployAdminTokenBank is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         address tokenAddress = 0xFadcc47E79575f09b6F7ED3936A2A3Eeb86d6E7D;
-        address adminAddress = 0x059dC4EEe9328A9f163a7e813B2f5B4A52ADD4dF;
+        address adminAddress = 0x260ef6029160BB9E89A50e0ead94DBf3F4725106;
 
         AdminTokenBank bank = new AdminTokenBank(adminAddress, tokenAddress);
 
